4d8b0b029aed3f228c3986172890b273277739bf
John Siegel <jpsiegel8@gmail.com>
2023-11-12 22:17:53 -0500

    Setup next web app

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+
+export const metadata: Metadata = {
+  title: "Math Collab",
+  description: "A collaborative tool for expressing numerical ideas",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html lang="en">
+      <body className={GeistSans.className}>{children}</body>
+    </html>
+  );
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default async function Home(): Promise<JSX.Element> {
+  return <h1>Hey!</h1>;
+}
